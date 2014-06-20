89d8858810054b4e661a983a23924e5def47b4fa
Labna <alban.cornuau@free.fr>
2014-06-20 14:19:54 +0200

    jquery+bootstrap

--- View/beginPage.php
+++ View/beginPage.php
@@ -5,10 +5,13 @@
 	<head>
 		<meta charset="utf-8" />
 		<title>Exia Store</title>
+		<link rel="stylesheet" type="text/css" href="View/css/bootstrap.min.css">
+		<script type="text/javascript" src="View/js/jquery.min.js"></script>
+		<script type="text/javascript" src="View/js/bootstrap.min.js"></script>
 	</head>
 
 	<body>
 
 		<header><?php include("View/banner.php"); ?></header>
 		<nav><?php include("View/nav.php") ?></nav>
-		
+
