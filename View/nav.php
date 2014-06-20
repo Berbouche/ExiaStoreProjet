8f4e25a41181f392db97863c77a26ca99c22c5f5
Berbouche <quentin.berbouche@gmail.com>
2014-06-20 15:01:46 +0200

    little bootstrap its a trap

--- View/nav.php
+++ View/nav.php
@@ -21,7 +21,7 @@
 	</li>
 </ul> -->
 
-<nav class="navbar navbar-default navbar-inverse" role="navigation">
+<nav class="navbar navbar-default" role="navigation">
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
