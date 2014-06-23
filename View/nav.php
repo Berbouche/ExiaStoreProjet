98ab1dd24c2be98220d53f4999dea20cbcd95fcc
emelineBitaud <emeline.bitaud@viacesi.fr>
2014-06-23 14:50:55 +0200

    bouton retour

--- View/nav.php
+++ View/nav.php
@@ -7,7 +7,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#"><span class="glyphicon glyphicon-home"> Home</span></a>
+      <a class="navbar-brand" href="http://localhost/ExiaStoreProjet"><span class="glyphicon glyphicon-home"> Home</span></a>
     </div>
 
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
