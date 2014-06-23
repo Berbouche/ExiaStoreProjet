f4a3909f8d8ba14eba8ac3ed4759442f8917dd87
Labna <alban.cornuau@free.fr>
2014-06-23 10:23:22 +0200

    MyAdmin

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
 	include("View/header.php");
 	include("View/menu.php");
+	if (isset($_GET['id'])) {
+		if ($_GET['id']="admin") {
+			include ("Controller/admin.php");
+		}
+	}
+
+
 	include("View/vitrine.php");
 	include("View/footer.php");
 ?>
