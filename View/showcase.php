1a3fe1a2cb966045c1724aee6d5e515833a449d7
Berbouche <quentin.berbouche@gmail.com>
2014-06-24 11:00:41 +0200

    monsieur

--- View/showcase.php
+++ View/showcase.php
@@ -14,28 +14,7 @@
     </div>
     <div id="collapseOne" class="panel-collapse collapse in">
       <div class="panel-body">
-        <!-- Debut de balise 1 -->
-<div class="row">
-  <div class="col-sm-6 col-md-4">
-    <div class="thumbnail">
-      <img src="View/image/compile.jpg" alt="compile">
-      <div class="caption">
-        <h3>So much CDs</h3>
-        <p>35 €</p>
-        <p>
-
-        <a href="#" class="btn btn-primary" role="button">Add to my ShopingCart</a>
-        <a id="info" onclick="javascript:show_hide('info');" class="btn btn-default" role="button">More Info</a>
-        <div id="info" class="info">
-        info of cd cd cd plin plin  plin 2 cd !!!
-        </div>
-        </p>
-
-      </div>
-    </div>
-  </div>
-</div>
-      <!-- Fin de balise 1 -->
+iiiiiiiiiiiiiiiiiiiiiii
       </div>
     </div>
   </div>
@@ -49,29 +28,7 @@
     </div>
     <div id="collapseTwo" class="panel-collapse collapse">
       <div class="panel-body">
-        <!-- Debut de balise 2 -->
-
-<div class="row">
-  <div class="col-sm-6 col-md-4">
-    <div class="thumbnail">
-      <img src="View/image/compile.jpg" alt="compile">
-      <div class="caption">
-        <h3>Other CD</h3>
-        <p>199.99$</p>
-        <p>
-
-        <a href="#" class="btn btn-primary" role="button">Add to my ShopingCart</a>
-        <a id="info" onclick="javascript:show_hide('info');" class="btn btn-default" role="button">More Info</a>
-        <div id="info" class="info">
-        info of cd cd cd plin plin  plin 2 cd !!!
-        </div>
-        </p>
-
-      </div>
-    </div>
-  </div>
-</div>
-      <!-- Fin de balise 2 -->
+       oooooooooooooooooooooooooooooooo
       </div>
     </div>
   </div>
@@ -85,34 +42,12 @@
     </div>
     <div id="collapseThree" class="panel-collapse collapse">
       <div class="panel-body">
-        <!-- Debut de balise 3 -->
-        <div class="row">
-  <div class="col-sm-6 col-md-4">
-    <div class="thumbnail">
-      <img src="View/image/compile.jpg" alt="compile">
-      <div class="caption">
-        <h3>Another CD</h3>
-        <p>0.99$</p>
-        <p>
-
-        <a href="#" class="btn btn-primary" role="button">Add to my ShopingCart</a>
-        <a id="info" onclick="javascript:show_hide('info');" class="btn btn-default" role="button">More Info</a>
-        <div id="info" class="info">
-        info of cd cd cd plin plin  plin 2 cd !!!
-        </div>
-        </p>
-
-      </div>
-    </div>
-  </div>
-</div>
-
-        <!-- Fin de balise 3 -->
+        uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu
       </div>
     </div>
   </div>
 
-  
+
 </div>
 
 
