42fc80d0adf018c643ba78288e6200fafb2c64a1
Labna <alban.cornuau@free.fr>
2014-06-24 09:25:50 +0200

    correct merge

--- View/header.php
+++ View/header.php
@@ -21,52 +21,6 @@
 	<body>
 		<header><?php include("View/banner.php"); ?></header>
 		<nav><?php include("View/nav.php") ?></nav>
-<<<<<<< HEAD
-        
-            <form id="formLog" method="post" action="register.php">
-                <p>
-                    <label for="email">Your e-mail :</label>
-                    <input type="email" name="email" id="email"/>
-                    
-                    <label for="password">Your password :</label>
-                    <input type="password" name="password" id="password"/>
-                    
-                    <button type="submit" value="Valide">OK</button>
-                <br>
-                <br />
-				</p>
-                
-                <h3><p>if you do not have an account, please register below...</p></h3>
-				<label for="nom"><br><br>Your Lastname </label>
-                <input type="text" name="nom" id="nom" />
-                <br>
-                <br />
-
-               <label for="prenom">Your Firstname </label>
-               <input type="text" name="prenom" id="prenom" />
-               <br>
-               <br />
-               <label for="pass">Your Password:</label>
-               <input type="password" name="pass" id="pass" />
-               <br>
-               <br />
-
-               <label for="email">Your e-mail </label>
-               <input type="email" name="email" id="email" />
-               <br>
-               <br />
-
-                <label for="date">Quel est votre date d'anniversaire ?</label>
-                <input type="date" name="birthday" id="birthday"/>
-                <br>
-                <br />
-       
-       
-       &nbsp;<input type="submit" value="Send" class="auto-style1" style="width: 90px" /></form>
-				
-				
-            </form>
-=======
     <form id="formLog" method="post" action="">
       <p>
       	<a href="?page=register">Not yet register</a>
@@ -77,4 +31,3 @@
         <button type="submit" value="Valide">OK</button>
       </p>
     </form>
->>>>>>> origin/master
