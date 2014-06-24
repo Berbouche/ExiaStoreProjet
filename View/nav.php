eac8df178280d3bee20c64b4cd99a07820ac5f24
elna1 <elna.tchiakpe@viacesi.fr>
2014-06-24 15:32:40 +0200

    cata

--- View/nav.php
+++ View/nav.php
@@ -16,8 +16,8 @@
               <a id="drop1" href="#" role="button" class="dropdown-toggle" data-toggle="dropdown">
 			  Catalog <b class="caret"></b></a>
               <ul class="dropdown-menu" role="menu" aria-labelledby="drop1">
-                <li role="presentation"><a role="menuitem" tabindex="-1" href="">CDs</a></li>
-                <li role="presentation"><a role="menuitem" tabindex="-1" href="">DVDs</a></li>
+                <li role="presentation"><a role="menuitem" tabindex="-1" href="">CD</a></li>
+                <li role="presentation"><a role="menuitem" tabindex="-1" href="">DVD</a></li>
                 <li role="presentation" class="divider"></li>
                 <li role="presentation"><a role="menuitem" tabindex="-1" href="">Other goodies</a></li>
               </ul>
