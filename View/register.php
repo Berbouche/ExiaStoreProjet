c3f6f4c1af8b8ea328c2185f200b0ac850b872ec
emelineBitaud <emeline.bitaud@viacesi.fr>
2014-06-24 09:37:42 +0200

    modif register

--- View/register.php
+++ View/register.php
@@ -1,14 +1,14 @@
 <?php?>    
 <form id="formRegister" method="post" action="register.php">
-	<label for="nom"><br><br>Your Lastname :</label>
-    <input type="text" name="nom" id="nom" /><br/><br/>
-    <label for="prenom">Your Firstname :</label>
-    <input type="text" name="prenom" id="prenom" /><br/><br/>
-    <label for="pass">Your Password :</label>
-    <input type="password" name="pass" id="pass" /><br><br />
+	<label for="lastname"><br><br>Your Lastname :</label>
+    <input type="text" name="lastname" id="lastname" /><br/><br/>
+    <label for="firstname">Your Firstname :</label>
+    <input type="text" name="firstname" id="firstname" /><br/><br/>
+    <label for="password">Your Password :</label>
+    <input type="password" name="password" id="password" /><br><br />
     <label for="email">Your e-mail :</label>
     <input type="email" name="email" id="email" /><br><br />  
-    <label for="date">Your birthday :</label>
+    <label for="birthday">Your birthday :</label>
     <input type="date" name="birthday" id="birthday"/><br/><br/>
     <input type="submit" value="Send" class="auto-style1" style="width: 90px" />
     </form>
