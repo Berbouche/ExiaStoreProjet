348bf81d5dca52cd6ecc16d1d1363944b5d45f2c
Berbouche <quentin.berbouche@gmail.com>
2014-06-24 11:41:52 +0200

    Promo

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
 	//echo(htmlspecialchars("$_SERVER[HTTP_HOST]", ENT_QUOTES, 'UTF-8'));
 	//echo "coucou".dirname("View/header.php")."coucou";
 
-	
+	include("View/sale.php");
 	include("View/vitrine.php");
 	}
 	include("View/footer.php");
