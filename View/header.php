4ec115c14899fb52a1f81827eeb740174ae4a674
emelineBitaud <emeline.bitaud@viacesi.fr>
2014-06-24 08:51:46 +0200

    modif

--- View/header.php
+++ View/header.php
@@ -39,28 +39,28 @@
                 
                 <h3><p>if you do not have an account, please register below...</p></h3>
 				<label for="nom"><br><br>Your Lastname </label>
-       <input type="text" name="nom" id="nom" />
-       <br>
-       <br />
+                <input type="text" name="nom" id="nom" />
+                <br>
+                <br />
 
-       <label for="prenom">Your Firstname </label>
-       <input type="text" name="prenom" id="prenom" />
-       <br>
-       <br />
-       <label for="pass">Your Password:</label>
-       <input type="password" name="pass" id="pass" />
-       <br>
-       <br />
- 
-       <label for="email">Your e-mail </label>
-       <input type="email" name="email" id="email" />
-       <br>
-       <br />
-        
-        <label for="date">Quel est votre date d'anniversaire ?</label>
-        <input type="date" name="birthday" id="birthday"/>
-        <br>
-        <br />
+               <label for="prenom">Your Firstname </label>
+               <input type="text" name="prenom" id="prenom" />
+               <br>
+               <br />
+               <label for="pass">Your Password:</label>
+               <input type="password" name="pass" id="pass" />
+               <br>
+               <br />
+
+               <label for="email">Your e-mail </label>
+               <input type="email" name="email" id="email" />
+               <br>
+               <br />
+
+                <label for="date">Quel est votre date d'anniversaire ?</label>
+                <input type="date" name="birthday" id="birthday"/>
+                <br>
+                <br />
        
        
        &nbsp;<input type="submit" value="Send" class="auto-style1" style="width: 90px" /></form>
