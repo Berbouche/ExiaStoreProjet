10a0886e3ecebee113518e7bd41a92f284b7a6cf
Berbouche <quentin.berbouche@gmail.com>
2014-06-24 08:51:24 +0200

    overview catalog

--- View/vitrine.php
+++ View/vitrine.php
@@ -1,3 +1,18 @@
+        
+<!-- Debut de balise 0 -->
+
+<div class="panel-group" id="accordion">
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
+          Item n°1
+        </a>
+      </h4>
+    </div>
+    <div id="collapseOne" class="panel-collapse collapse in">
+      <div class="panel-body">
+        <!-- Debut de balise 1 -->
 <div class="row">
   <div class="col-sm-6 col-md-4">
     <div class="thumbnail">
@@ -8,33 +23,84 @@
         <p>
 
         <a href="#" class="btn btn-primary" role="button">Add to my ShopingCart</a>
-        
-
-
-<div class="panel-group" id="accordion">
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h4 class="panel-title">
-        <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
-          More Info
-        </a>
-      </h4>
-    </div>
-    <div id="collapseOne" class="panel-collapse collapse in">
-      <div class="panel-body">
-       This is much CD for you for sale. ONLY 199.99$ !
-      </div>
-    </div>
-  </div>
-</div>
-
-
         </p>
 
       </div>
     </div>
   </div>
 </div>
+      <!-- Fin de balise 1 -->
+      </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
+          Item n°2
+        </a>
+      </h4>
+    </div>
+    <div id="collapseTwo" class="panel-collapse collapse">
+      <div class="panel-body">
+        <!-- Debut de balise 2 -->
+
+<div class="row">
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="View/image/compile.jpg" alt="compile">
+      <div class="caption">
+        <h3>Other CD</h3>
+        <p>199.99$</p>
+        <p>
+
+        <a href="#" class="btn btn-primary" role="button">Add to my ShopingCart</a>
+        </p>
+
+      </div>
+    </div>
+  </div>
+</div>
+      <!-- Fin de balise 2 -->
+      </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
+          Item n°3
+        </a>
+      </h4>
+    </div>
+    <div id="collapseThree" class="panel-collapse collapse">
+      <div class="panel-body">
+        <!-- Debut de balise 3 -->
+        <div class="row">
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="View/image/compile.jpg" alt="compile">
+      <div class="caption">
+        <h3>Another CD</h3>
+        <p>0.99$</p>
+        <p>
+
+        <a href="#" class="btn btn-primary" role="button">Add to my ShopingCart</a>
+        </p>
+
+      </div>
+    </div>
+  </div>
+</div>
+
+        <!-- Fin de balise 3 -->
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Fin de balise 0 -->
+        
 
 <?php
 	include ("Model/catalog.php");
